3a7c3852c8a68d3521e08b00a57be149aaeba4c4
cameron <hammerbro_97@hotmail.com>
2023-04-01 10:27:20 -0600

    added pokemon selection screen

---
+++ js/app.js
@@ -0,0 +1,86 @@
+let pokemon = [{
+
+
+    name: ` Magikarp`,
+
+    image_url: `/images/magikarp.jpg`
+
+},
+
+{
+
+
+    name: `Rayquaza`,
+
+    image_url: `/images/rayqauza.png`
+
+
+},
+
+{
+
+
+    name: `Haunter`,
+
+    image_url: `/images/haunter.png`
+}
+
+
+];
+
+let pokemon_json = Cookies.get(`selection`);
+
+selection_pokemon = [];
+
+function choose_your_pokemon(pokemon_choice){
+
+    let pokemons = {
+
+        name: `${pokemon_choice[`target`].getAttribute(`pokemon_name`)}`,
+
+        image_url: `${pokemon_choice[`target`].getAttribute(`pokemon_image`)}`
+
+    }
+
+selection_pokemon.push(pokemons);
+pokemon_json = JSON.stringify(selection_pokemon);
+Cookies.set(`selection`, pokemon_json);
+
+};
+
+
+let choose_your_pokemon_page = document.querySelector(`#section_main`);
+
+for(let counter = 0; counter < pokemon.length; counter = counter +1){
+
+choose_your_pokemon_page.insertAdjacentHTML(`beforeend`, 
+
+
+`<article>
+<span class="main_span">
+
+<h2>${pokemon[counter][`name`]}</h3>
+
+<img src="${pokemon[counter][`image_url`]}">
+
+<a href="/pages/battle.html"><button class="clickme"
+
+pokemon_name="${pokemon[counter][`name`]}"
+
+pokemon_image="${pokemon[counter][`image_url`]}"
+
+>CHOOSE POKEMON</button></a>
+
+
+</span>
+</article>
+
+`);
+
+};
+
+let chosen_pokemon = document.querySelectorAll(`.clickme`);
+
+for(let counter = 0; counter < chosen_pokemon.length; counter = counter +1){
+    chosen_pokemon[counter].addEventListener(`click`, choose_your_pokemon);
+}
