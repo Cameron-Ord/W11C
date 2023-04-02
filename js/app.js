bdac747bb41018bc25d5a96543a760e36e106abc
cameron <hammerbro_97@hotmail.com>
2023-04-02 15:16:51 -0600

    added enemy

--- js/app.js
+++ js/app.js
@@ -64,11 +64,13 @@ function choose_your_pokemon(pokemon_choice) {
     }
 
     selection_pokemon.push(pokemons);
+
     let pokemon_json = JSON.stringify(selection_pokemon);
+
     Cookies.set(`selection`, pokemon_json);
 
     Cookies.set(`player_health`, 150);
-    Cookies.set(`computer_health`, 200);
+
 
 
 
@@ -103,17 +105,90 @@ pokemon_damage"${pokemons[counter].dmg}"
 
 };
 
-let rival ={
+let rival =[{
 
 
+    name: `deoxsys`,
 
-    
+    health : `250`,
+
+    image_url: `/images/deoxsys.jpg`,
+
+    dmg: `25`
+
+
 }
+];
+
+let enemy_json = Cookies.get(`enemy`);
+
+let enemy_selection = [];
+
+function choose_enemy(enemy){
 
 
+    let rival = {
+
+        name: `${enemy[`target`].getAttribute(`enemy_name`)}`,
+
+        image_url: `${enemy[`target`].getAttribute(`enemy_image`)}`,
+
+        health: `${enemy[`target`].getAttribute(`enemy_health`)}`,
+
+        dmg: `${enemy[`target`].getAttribute(`enemy_damage`)}`
+
+
+
+    }
+
+    enemy_selection.push(rival);
+
+    let enemy_json = JSON.stringify(enemy_selection);
+
+    Cookies.set(`enemy`, enemy_json);
+
+    Cookies.set(`computer_health`, 200);
+};
+
+
+for(counter = 0; counter < rival.length; counter = counter +1){
+
+
+    choose_your_pokemon_page.insertAdjacentHTML(`beforeend`,
+    
+    `<article>
+    <span class="main_span">
+    
+    <h2>${rival[counter].name}</h3>
+    
+    <img src="${rival[counter].image_url}">
+ 
+    <button class="click_enemy"
+    
+    enemy_name="${rival[counter].name}"
+    enemy_health="${rival[counter].health}"
+    enemy_image="${rival[counter].image_url}"
+    enemy_damage"${rival[counter].dmg}"
+    
+    >CHOOSE ENEMY</button>
+    
+    
+    </span>
+    </article>
+    `)
+
+}
 
 let chosen_pokemon = document.querySelectorAll(`.clickme`);
 
 for (let counter = 0; counter < chosen_pokemon.length; counter = counter + 1) {
     chosen_pokemon[counter].addEventListener(`click`, choose_your_pokemon);
+
+}
+
+let chosen_enemy = document.querySelectorAll(`.click_enemy`);
+
+for (let counter = 0; counter < chosen_enemy.length; counter = counter + 1) {
+    chosen_enemy[counter].addEventListener(`click`, choose_enemy);
+
 }
